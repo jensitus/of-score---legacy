5e46bf57ca01d19932948d43f8b1cc3107823f7d
Jens <jens@ist-ur.org>
2019-12-21 20:22:35 +0100

    sintering

--- src/app/of/three/three.component.ts
+++ src/app/of/three/three.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {OfScore} from '../of-score.enum';
 
 @Component({
   selector: 'app-three',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThreeComponent implements OnInit {
 
+  title: string;
+  bg: string;
+
+  yes = 'yes';
+  no = 'no';
+  button: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.title = 'sintering';
+    this.bg = 'light';
+    this.button = localStorage.getItem(OfScore.THREE);
+  }
+
+  select(button) {
+    console.log(button);
+    switch (button) {
+      case 'yes': {
+        this.button = this.yes;
+        localStorage.setItem(OfScore.THREE, this.yes);
+        break;
+      }
+      case 'no': {
+        this.button = this.no;
+        localStorage.setItem(OfScore.THREE, this.no);
+        break;
+      }
+    }
+
   }
 
 }
