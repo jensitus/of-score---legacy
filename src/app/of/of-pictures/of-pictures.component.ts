bcee680a1c1d657ebf3343ad8e2dcfdca37d8ff3
Jens <jens@ist-ur.org>
2020-02-03 21:55:32 +0100

    Pictures for Morphology title in Modal

--- src/app/of/of-pictures/of-pictures.component.ts
+++ src/app/of/of-pictures/of-pictures.component.ts
@@ -17,12 +17,14 @@ export class OfPicturesComponent implements OnInit {
   private _OF_2_O_T = '../../assets/pictures/of-pictures/OF2oT.jpg';
 
   private _OF_3_M_T = '../../assets/pictures/of-pictures/OF3mT.jpeg';
+  private _OF_3_O_T = '../../assets/pictures/of-pictures/OF3oT.jpeg';
 
-  private _OF_4_M_T = '../../assets/pictures/of-pictures/OF4mT.jpeg';
+  private _OF_4_O_T = '../../assets/pictures/of-pictures/OF4oT.jpeg';
 
-  private _OF_5_M_T = '../../assets/pictures/of-pictures/OF5mT.jpeg';
+  private _OF_5_O_T = '../../assets/pictures/of-pictures/OF5oT.jpeg';
 
   ofPics: string[];
+  title: string;
   pauseOnHover = true;
 
   constructor(
@@ -33,23 +35,29 @@ export class OfPicturesComponent implements OnInit {
   ngOnInit() {
     switch (this.ofp) {
       case '1': {
-        this.ofPics = [this._OF_1_M_T, this._OF_1_O_T];
+        this.ofPics = [this._OF_1_O_T];
+        this.title = 'OF 1';
         break;
       }
       case '2': {
-        this.ofPics = [this._OF_2_M_T, this._OF_2_O_T];
+        this.ofPics = [this._OF_2_O_T];
+        this.title = 'OF 2';
         break;
       }
       case '3': {
-        this.ofPics = [this._OF_3_M_T];
+        this.ofPics = [this._OF_3_O_T];
+        this.title = 'OF 3';
         break;
       }
       case '4': {
-        this.ofPics = [this._OF_4_M_T];
+        this.ofPics = [this._OF_4_O_T];
+        this.title = 'OF 4';
         break;
       }
       case '5': {
-        this.ofPics = [this._OF_5_M_T];
+        this.ofPics = [this._OF_5_O_T];
+        this.title = 'OF 5';
+        break;
       }
     }
 
